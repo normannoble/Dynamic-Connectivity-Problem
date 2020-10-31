40aedc418d4609b211acf88a5595a58d68346ec1
repl.it user <replituser@example.com>
2020-10-31 09:44:01 +0000

    Adding functionality to call initialise and call QuickFind

--- Main.java
+++ Main.java
@@ -1,5 +1,27 @@
-class Main {
+public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    QuickFind quickFind = new QuickFind(10);
+    // long startTime = System.nanoTime();
+    createConnections(quickFind);
+    
+    System.out.println(quickFind.connected(4,3));
+    // long stopTime = System.nanoTime();
+    //System.out.println("Execution Time: " + (stopTime - startTime));
+
+    int[] nodes = quickFind.nodes();
+    for (int i = 0; i < nodes.length; i++){
+      System.out.println(nodes[i]);
+    }
+  }
+
+  public static void createConnections(QuickFind quickFind){
+    quickFind.union(4,3);
+    quickFind.union(3,8);
+    quickFind.union(6,5);
+    quickFind.union(9,4);
+    quickFind.union(2,1);
+    quickFind.union(5,0);
+    quickFind.union(7,2);
+    quickFind.union(6,1);
   }
 }
--- QuickFind.java
+++ QuickFind.java
@@ -1,3 +1,7 @@
+/*  This class starts by creating an array of nodes where each node belongs to a unique collection defined by its value.
+    When a node is joined to another node, they are added to the same collection which is identified by the value of the second node. This continues for each subsequent union meaning that the array is affectivley reindexing the collections each time a union is performed. This would make the algo quick to check if array positions are in the same collection but slow to join them together.
+*/
+
 public class QuickFind
 {
   private int[] id;
@@ -12,11 +16,16 @@ public class QuickFind
   public boolean connected(int p, int q)
   { return id[p] == id[q]; }
  
-  public void union(int p, int q)
+  public void union(int nodeAKey, int nodeBKey)
   {
-    int pid = id[p];
-    int qid = id[q];
+    int nodeAValue = id[nodeAKey];
+    int nodeBValue = id[nodeBKey];
+
     for (int i = 0; i < id.length; i++)
-    if (id[i] == pid) id[i] = qid;
+    if (id[i] == nodeAValue) id[i] = nodeBValue;
+ }
+
+ public int[] nodes(){
+   return id;
  }
 }
