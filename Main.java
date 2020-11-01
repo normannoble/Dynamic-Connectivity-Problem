ce6a8d3af76a51fd03222bf0a55220403c7549ed
repl.it user <replituser@example.com>
2020-11-01 04:39:32 +0000

    Tidy up Main implementation

--- Main.java
+++ Main.java
@@ -1,38 +1,24 @@
 public class Main {
   public static void main(String[] args) {
-    //QuickFind quickFind = new QuickFind(10);
-    //createQFConnections(quickFind);
-    //System.out.println(quickFind.connected(4, 3));
 
-    QuickUnionWeighted quickUnion = new QuickUnionWeighted(10);
-    createQUCOnnections(quickUnion);
-    System.out.println(quickUnion.connected(4, 3));
+    IUnionFind unionFind = new QuickUnionWeighted(10);
+    createConnections(unionFind);
+    System.out.println(unionFind.connected(4, 3));
 
-    int[] nodes = quickUnion.nodes();
+    int[] nodes = unionFind.nodes();
     for (int i = 0; i < nodes.length; i++) {
       System.out.print(nodes[i]+",");
     }
   }
 
-  public static void createQUCOnnections(QuickUnionWeighted quickUnion) {
-    quickUnion.union(4, 3);
-    quickUnion.union(3, 8);
-    quickUnion.union(3, 7);
-    quickUnion.union(4, 2);
-    quickUnion.union(2, 1);
+  public static void createConnections(IUnionFind unionFind) {
+    unionFind.union(4, 3);
+    unionFind.union(3, 8);
+    unionFind.union(3, 7);
+    unionFind.union(4, 2);
+    unionFind.union(2, 1);
     //quickUnion.union(5, 0);
     //quickUnion.union(7, 2);
     //quickUnion.union(6, 1);
-  }
-
-  public static void createQFConnections(QuickFind quickFind) {
-    quickFind.union(4, 3);
-    quickFind.union(3, 8);
-    quickFind.union(6, 5);
-    quickFind.union(9, 4);
-    quickFind.union(2, 1);
-    quickFind.union(5, 0);
-    quickFind.union(7, 2);
-    quickFind.union(6, 1);
   }
 }
