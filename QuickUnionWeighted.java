5328572a3cb2da7494ec55171b6006351bbe91db
repl.it user <replituser@example.com>
2020-11-01 04:16:16 +0000

    Add Weighted Quick Union algo

---
+++ QuickUnionWeighted.java
@@ -0,0 +1,37 @@
+public class QuickUnionWeighted {
+  private int[] id;
+  private int[] sz;
+
+  public QuickUnionWeighted(int N) {
+    id = new int[N];
+    for (int i = 0; i < N; i++)
+      id[i] = i;
+  }
+
+  private int root(int i) {
+    // while nodeValue is not the same as nodeKey (i.e. not at root)
+    while (i != id[i])
+      // nodeValue changes to next node up the chain
+      i = id[i];
+    // return when root is reached.
+    return i;
+  }
+
+  public boolean connected(int p, int q) {
+    return root(p) == root(q);
+  }
+
+  public void union(int p, int q) {
+    // find the root of both
+    int i = root(p);
+    int j = root(q);
+
+    if (i == j) return;
+    if (sz[i] < sz[j]) { id[i] = j; sz[j] += sz[i]; }
+    else { id[j] = i; sz[i] += sz[j]; } 
+  }
+
+  public int[] nodes(){
+   return id;
+ }
+}
